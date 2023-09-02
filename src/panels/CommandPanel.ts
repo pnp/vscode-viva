0cb1e415111463d74b6f1633b97ba744e7cd364b
Adam Wójcik <58668583+Adam-it@users.noreply.github.com>
2023-09-02 22:40:51 +0200

    Refactors help and feedback section. Closes: #77 (#78)
    ## 🎯 Aim
    
    The aim is to refactor help and feedbakc section to reoder links and add
    more learning path links
    
    ## 📷 Result
    
    
    ![image](https://github.com/pnp/vscode-viva/assets/58668583/56986378-1a85-4c32-b937-326971cad3f0)
    
    
    ## ✅ What was done
    
    - [X] Refactored the help links
    
    ## 🔗 Related issue
    
    Closes: #77

--- src/panels/CommandPanel.ts
+++ src/panels/CommandPanel.ts
@@ -173,29 +173,9 @@ export class CommandPanel {
   private static helpTreeView() {
     const links = [
       {
-        title: 'Viva Connections Extensibility',
-        url: 'https://docs.microsoft.com/en-us/sharepoint/dev/spfx/viva/overview-viva-connections',
+        title: 'Overview of Viva Connections Extensibility',
+        url: 'https://learn.microsoft.com/en-us/sharepoint/dev/spfx/viva/overview-viva-connections',
         image: { name: 'book', custom: false }
-      },
-      {
-        title: 'Viva Connections Design Guidance',
-        url: 'https://docs.microsoft.com/en-us/sharepoint/dev/spfx/viva/design/design-intro',
-        image: { name: 'book', custom: false }
-      },
-      {
-        title: 'Viva Connections samples',
-        url: 'https://adoption.microsoft.com/sample-solution-gallery?sortby=creationDateTime-true&keyword=&product=Viva&action=ajax_plugin_call_sample_solution_gallery',
-        image: { name: 'library', custom: false }
-      },
-      {
-        title: 'Learning path: Microsoft Viva Connections',
-        url: 'https://docs.microsoft.com/en-us/learn/modules/viva-connections-get-started/',
-        image: { name: 'mortar-board', custom: false }
-      },
-      {
-        title: 'Adaptive Card Designer',
-        url: 'https://adaptivecards.io/designer/',
-        image: { name: 'globe', custom: false }
       },
       {
         title: 'Overview of the SharePoint Framework',
@@ -203,19 +183,44 @@ export class CommandPanel {
         image: { name: 'book', custom: false }
       },
       {
-        title: 'Join the Microsoft 365 Developer Program',
-        url: 'https://developer.microsoft.com/en-us/microsoft-365/dev-program',
-        image: { name: 'star-empty', custom: false }
+        title: 'Overview of Microsoft Graph',
+        url: 'https://learn.microsoft.com/en-us/graph/overview?view=graph-rest-1.0',
+        image: { name: 'book', custom: false }
       },
       {
-        title: 'Microsoft Graph REST API Documentation',
-        url: 'https://docs.microsoft.com/en-us/graph/api/overview?view=graph-rest-1.0',
-        image: { name: 'book', custom: false }
+        title: 'Learning path: Extend Microsoft Viva Connections',
+        url: 'https://learn.microsoft.com/en-us/training/paths/m365-extend-viva-connections/',
+        image: { name: 'mortar-board', custom: false }
+      },
+      {
+        title: 'Learning path: Extend Microsoft SharePoint - Associate',
+        url: 'https://learn.microsoft.com/en-us/training/paths/m365-sharepoint-associate/',
+        image: { name: 'mortar-board', custom: false }
+      },
+      {
+        title: 'Learning path: Microsoft Graph Fundamentals',
+        url: 'https://learn.microsoft.com/en-us/training/paths/m365-msgraph-fundamentals/',
+        image: { name: 'mortar-board', custom: false }
+      },
+      {
+        title: 'Sample Solution Gallery',
+        url: 'https://adoption.microsoft.com/en-us/sample-solution-gallery/',
+        image: { name: 'library', custom: false }
+      },
+      {
+        title: 'Adaptive Card Designer',
+        url: 'https://adaptivecards.io/designer/',
+        image: { name: 'globe', custom: false }
       },
       {
         title: 'Microsoft Graph Explorer',
         url: 'https://developer.microsoft.com/en-us/graph/graph-explorer',
         image: { name: 'globe', custom: false }
+      },
+      {
+        title: 'Join the Microsoft 365 Developer Program',
+        url: 'https://developer.microsoft.com/en-us/microsoft-365/dev-program',
+        image: { name: 'star-empty', custom: false }
       },
       {
         title: 'Microsoft 365 & Power Platform Community Home',
@@ -226,6 +231,11 @@ export class CommandPanel {
         title: 'Join the Microsoft 365 & Power Platform Community Discord Server',
         url: 'https://aka.ms/community/discord',
         image: { name: 'feedback', custom: false }
+      },
+      {
+        title: 'Wiki',
+        url: 'https://github.com/pnp/vscode-viva/wiki/How-to',
+        image: { name: 'question', custom: false }
       },
       {
         title: 'Report an issue',
