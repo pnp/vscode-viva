caf6bb2cfea0b432dfaa51e675365c7258715ce2
DevPio <luccaspio890@gmail.com>
2024-06-18 01:44:32 -0300

    feat: :sparkles: add selected filters and clear filters button
    add selected filters and clear filters button - maybe similar to TT add tags that represent each filter set with a button to clear all picked filters

--- src/webview/view/components/gallery/SearchBar.tsx
+++ src/webview/view/components/gallery/SearchBar.tsx
@@ -1,10 +1,9 @@
-import { VSCodeCheckbox, VSCodeTextField } from '@vscode/webview-ui-toolkit/react';
+import { VSCodeCheckbox, VSCodeTextField,VSCodeTag } from '@vscode/webview-ui-toolkit/react';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { SearchIcon } from '../icons';
 import { IDropdownOption } from '@fluentui/react';
 import { MultiSelect } from '../controls';
-
 
 export interface ISearchBarProps {
   onSearchTextboxChange: (event: any) => void;
@@ -13,26 +12,46 @@ export interface ISearchBarProps {
   onFilterOnlyScenariosChange: (event: any) => void;
   initialQuery?: string;
   spfxVersions: IDropdownOption[];
+  selectedFilters:ISelectedFilter[];
+  onRemoveFilterBySPFxVersion: (key:string) => void;
+  onRemoveFilterByComponentType: (key:string) => void;
+  clearAllFilters: () => void;
+  onClearTextboxChange: () => void;
+  showOnlyScenarios: boolean;
 }
 
-export const SearchBar: React.FunctionComponent<ISearchBarProps> = ({ onSearchTextboxChange, onFilterBySPFxVersionChange, onFilterByComponentTypeChange, onFilterOnlyScenariosChange, initialQuery, spfxVersions }: React.PropsWithChildren<ISearchBarProps>) => {
+export interface ISelectedFilter {
+  key:string | null;
+  kind:'spfxVersion'|'componentType'
+}
+
+export const SearchBar: React.FunctionComponent<ISearchBarProps> = ({ onSearchTextboxChange, onFilterBySPFxVersionChange, onFilterByComponentTypeChange, onFilterOnlyScenariosChange, initialQuery, spfxVersions, selectedFilters,onRemoveFilterByComponentType, onRemoveFilterBySPFxVersion,clearAllFilters, onClearTextboxChange,showOnlyScenarios }: React.PropsWithChildren<ISearchBarProps>) => {
   const [query, setQuery] = useState<string>('');
 
   useEffect(() => {
     setQuery(initialQuery ?? '');
   }, [initialQuery]);
 
-  const getComponentTypes = (): IDropdownOption[] => {
-    const componentTypes: IDropdownOption[] = [
+  const getComponentTypeOptions = (): IDropdownOption[] => {
+    const options: IDropdownOption[] = [
       { key: 'webpart', text: 'Web Part' },
       { key: 'extension', text: 'Extension' },
       { key: 'adaptiveCardExtension', text: 'ACE' }
     ];
 
-    return componentTypes;
+    selectedFilters.forEach(filter => {
+      if (filter.kind === 'componentType') {
+        const matchingOption = options.find(option => option.key === filter.key);
+        if (matchingOption) {
+          matchingOption.selected = true;
+        }
+      }
+    });
+
+    return options;
   };
 
-  const componentTypes = getComponentTypes();
+  const componentTypes = getComponentTypeOptions();
 
   return (
     <div>
@@ -51,8 +70,23 @@ export const SearchBar: React.FunctionComponent<ISearchBarProps> = ({ onSearchTe
           <MultiSelect options={componentTypes} label="Component Type" onChange={onFilterByComponentTypeChange}/>
         </div>
         <div>
-          <VSCodeCheckbox onChange={onFilterOnlyScenariosChange}>show only scenarios</VSCodeCheckbox>
+          <VSCodeCheckbox checked={showOnlyScenarios} onChange={onFilterOnlyScenariosChange}>show only scenarios</VSCodeCheckbox>
         </div>
+      </div>
+
+      <div className={'flex flex-wrap'}>
+        {query && <div className={'p-1 cursor-pointer'} onClick={() => {
+          setQuery('');
+          onClearTextboxChange();
+        }}><VSCodeTag >{query} <strong>X</strong></VSCodeTag></div>}
+        {selectedFilters.length > 0 && selectedFilters.map((filter, index) => {
+          if (filter.kind === 'spfxVersion') {
+            return <div className={'p-1 cursor-pointer'} onClick={() => onRemoveFilterBySPFxVersion(filter.key as string)}><VSCodeTag key={index} >{filter.key} <strong>X</strong></VSCodeTag></div>;
+          }
+
+          return <div className={'p-1 cursor-pointer'} onClick={() => onRemoveFilterByComponentType(filter.key as string)}><VSCodeTag key={index} >{filter.key} <strong>X</strong></VSCodeTag></div>;
+        })}
+        {(selectedFilters.length > 0 || query)&& <div className={'p-1 cursor-pointer'} onClick={clearAllFilters}><strong style={{color:'#3664be'}}>Clear all</strong></div>}
       </div>
     </div>
   );
