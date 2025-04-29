d0fea95e5158578db3950105fa4d4dd92ac4ddb4
Adam Wójcik <adam.wojcik.it@gmail.com>
2025-04-30 00:40:18 +0200

    Updates devDependencies to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   content: [
-    "./src/webview/view/**/*.{js,jsx,ts,tsx}",
+    './src/webview/view/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  future: {
+    standardFontWeights: true,
+  },
+  corePlugins: {
+    preflight: true,
+  }
+};
