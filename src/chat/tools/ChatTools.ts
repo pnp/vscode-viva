9361c2f5b0a1393d174bf3757f19433a8c5fc24d
Saurabh Tripathi <saurabh7019@gmail.com>
2025-09-23 21:29:36 +0200

    Rework and add more SPO app management LM Tools. Closes #489

--- src/chat/tools/ChatTools.ts
+++ src/chat/tools/ChatTools.ts
@@ -1,7 +1,7 @@
 import { lm } from 'vscode';
 import { Subscription } from '../../models';
 import { Extension } from '../../services/dataType/Extension';
-import { SharePointAppInstall, SharePointAppList, SharePointListAdd, SharePointListGet, SharePointListRemove, SharePointPageAdd, SharePointSiteAdd, SharePointSiteGet, SharePointSiteRemove } from './spo/index';
+import { SharePointAppInstall, SharePointAppInstanceList, SharePointAppList, SharePointAppUninstall, SharePointAppUpgrade, SharePointListAdd, SharePointListGet, SharePointListRemove, SharePointPageAdd, SharePointSiteAdd, SharePointSiteGet, SharePointSiteRemove } from './spo/index';
 import { SharePointFrameworkProjectUpgrade } from './spfx';
 
 
@@ -10,31 +10,40 @@ export class ChatTools {
         const subscriptions: Subscription[] = Extension.getInstance().subscriptions;
 
         subscriptions.push(
-            lm.registerTool('spo_app_list', new SharePointAppList())
+            lm.registerTool('install_spo_app', new SharePointAppInstall())
         );
         subscriptions.push(
-            lm.registerTool('spo_app_install', new SharePointAppInstall())
+            lm.registerTool('list_spo_app_instances', new SharePointAppInstanceList())
         );
         subscriptions.push(
-            lm.registerTool('spo_list_add', new SharePointListAdd())
+            lm.registerTool('list_spo_app', new SharePointAppList())
         );
         subscriptions.push(
-            lm.registerTool('spo_list_get', new SharePointListGet())
+            lm.registerTool('uninstall_spo_app', new SharePointAppUninstall())
         );
         subscriptions.push(
-            lm.registerTool('spo_list_remove', new SharePointListRemove())
+            lm.registerTool('upgrade_spo_app', new SharePointAppUpgrade())
+        );
+        subscriptions.push(
+            lm.registerTool('add_spo_list', new SharePointListAdd())
+        );
+        subscriptions.push(
+            lm.registerTool('get_spo_list', new SharePointListGet())
+        );
+        subscriptions.push(
+            lm.registerTool('remove_spo_list', new SharePointListRemove())
         );
         subscriptions.push(
             lm.registerTool('add_spo_page', new SharePointPageAdd())
         );
         subscriptions.push(
-            lm.registerTool('spo_site_add', new SharePointSiteAdd())
+            lm.registerTool('add_spo_site', new SharePointSiteAdd())
         );
         subscriptions.push(
-            lm.registerTool('spo_site_remove', new SharePointSiteRemove())
+            lm.registerTool('get_spo_site', new SharePointSiteGet())
         );
         subscriptions.push(
-            lm.registerTool('spo_site_get', new SharePointSiteGet())
+            lm.registerTool('remove_spo_site', new SharePointSiteRemove())
         );
         subscriptions.push(
             lm.registerTool('upgrade_spfx_project', new SharePointFrameworkProjectUpgrade())
