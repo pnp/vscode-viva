0ef8c62825312ea528aca7e4fefb6210d326050f
Saurabh Tripathi <saurabh7019@gmail.com>
2025-10-07 21:21:39 +0200

    Rework and add more SPO app management LM Tools. Closes #489 (#584)
    ## 🎯 Aim
    
    > Refactors existing and Adds more SPO app management LM Tools
    
    ## 📷 Result
    
    <img width="770" height="550" alt="image"
    src="https://github.com/user-attachments/assets/b8bf910b-17d2-475c-9b1a-c1f6a0694686"
    />
    
    ## ✅ What was done
    
    > clarify what was done and what still needs to be finished ex. [Remove
    this line]
    
    - [X] reword the tool name to follow the naming {verb}_{noun}
    - [X] improve model description - list_spo_app, install_spo_app
    - [X] enhanced list_spo_app to support listing apps from site collection
    app catalog
    - [X] enhanced install_spo_app to install app from site level app
    catalog
    - [X] added new tools - uninstall_spo_app, upgrade_spo_app,
    list_spo_app_instances
    - [ ] when app is already installed, upgrade cli command won't run
    automatically
    
    ## 🔗 Related issue
    
    Closes: #489
    
    ---------
    
    Co-authored-by: Adam Wójcik <adam.wojcik.it@gmail.com>

---
+++ src/chat/tools/spo/app/AppInstanceList.ts
@@ -0,0 +1,45 @@
+import { CancellationToken, LanguageModelTextPart, LanguageModelTool, LanguageModelToolInvocationOptions, LanguageModelToolInvocationPrepareOptions, LanguageModelToolResult, MarkdownString } from 'vscode';
+import { CliExecuter } from '../../../../services/executeWrappers/CliCommandExecuter';
+import { validateAuth } from '../utils/ToolAuthValidationUtil';
+
+
+interface ISharePointAppInstanceListParameters {
+    siteUrl: string;
+}
+
+export class SharePointAppInstanceList implements LanguageModelTool<ISharePointAppInstanceListParameters> {
+    async invoke(
+        options: LanguageModelToolInvocationOptions<ISharePointAppInstanceListParameters>,
+        _token: CancellationToken
+    ) {
+        const params = options.input;
+        const authValidationResult = await validateAuth();
+        if (authValidationResult !== true) {
+            return authValidationResult as LanguageModelToolResult;
+        }
+
+        const result = await CliExecuter.execute('spo app instance list', 'json', params);
+        if (result.stderr) {
+            return new LanguageModelToolResult([new LanguageModelTextPart(`Error: ${result.stderr}`)]);
+        }
+
+        return new LanguageModelToolResult([new LanguageModelTextPart(`Installed apps from ${params.siteUrl} retrieved successfully ${(result.stdout !== '' ? `\nResult: ${result.stdout}` : '')}`)]);
+    }
+
+    async prepareInvocation(
+        options: LanguageModelToolInvocationPrepareOptions<ISharePointAppInstanceListParameters>,
+        _token: CancellationToken
+    ) {
+        const params = options.input;
+
+        const confirmationMessages = {
+            title: `List installed apps from ${params.siteUrl}`,
+            message: new MarkdownString(`Should I retrieve all installed apps from ${params.siteUrl}?`),
+        };
+
+        return {
+            invocationMessage: `Getting installed apps from ${params.siteUrl}`,
+            confirmationMessages,
+        };
+    }
+}
