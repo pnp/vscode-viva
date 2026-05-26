3700273704609c1caf9bf9d69502dc9100e813b3
Adam-it <adam.wojcik.it@gmail.com>
2026-05-26 22:50:01 +0200

    Updates astro config for SPFx Toolkit docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/vscode-viva/',
 	output: 'static',
 	integrations: [
 		starlight({
