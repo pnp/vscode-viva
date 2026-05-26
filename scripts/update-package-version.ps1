a5d3e0020f292f977cb02893b8f90016a06f1eaf
Saurabh Tripathi <saurabh7019@gmail.com>
2026-05-26 16:39:24 +0200

    fix: remove hardcoded path in update-package-version script

--- scripts/update-package-version.ps1
+++ scripts/update-package-version.ps1
@@ -3,9 +3,9 @@ param (
     [string]$PathToWorkspace
 )
 
-$packageJson = Get-Content "$PathToWorkspace\vscode-viva\package.json" | ConvertFrom-Json
+$packageJson = Get-Content "$PathToWorkspace\package.json" | ConvertFrom-Json
 $oldVersion = $packageJson.version
 $oldVersionParts = $oldVersion.Split(".")
 $newVersion = "$($oldVersionParts[0]).$($oldVersionParts[1]).$([int]$oldVersionParts[2] + 1)"
 $packageJson.version = $newVersion
-$packageJson | ConvertTo-Json -Depth 10 | Out-File "$PathToWorkspace\vscode-viva\package.json" -Encoding utf8 -Force
+$packageJson | ConvertTo-Json -Depth 10 | Out-File "$PathToWorkspace\package.json" -Encoding utf8 -Force
