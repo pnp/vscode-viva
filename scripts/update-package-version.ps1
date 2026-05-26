f678157d959419d53c441cc03d64a519c570daa3
Saurabh Tripathi <saurabh7019@gmail.com>
2026-05-26 15:57:30 +0200

    uses native GitHub CLI to create PR. Aligned with update-samples

--- scripts/update-package-version.ps1
+++ scripts/update-package-version.ps1
@@ -8,4 +8,4 @@ $oldVersion = $packageJson.version
 $oldVersionParts = $oldVersion.Split(".")
 $newVersion = "$($oldVersionParts[0]).$($oldVersionParts[1]).$([int]$oldVersionParts[2] + 1)"
 $packageJson.version = $newVersion
-$packageJson | ConvertTo-Json  -Depth 10 | Out-File "$PathToWorkspace\vscode-viva\package.json" -Encoding utf8 -Force
+$packageJson | ConvertTo-Json -Depth 10 | Out-File "$PathToWorkspace\vscode-viva\package.json" -Encoding utf8 -Force
